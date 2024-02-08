2f5caffde89f055bee83a55e534d48a6e8d76e27
Lethe <37437842+0xlethe@users.noreply.github.com>
2024-02-08 16:05:50 +0900

    fix: iOS Pod install issue

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -3,6 +3,9 @@ const pak = require('../package.json');
 
 module.exports = {
   dependencies: {
+    ...(process.env.NO_FLIPPER
+      ? {'react-native-flipper': {platforms: {ios: null}}}
+      : {}),
     [pak.name]: {
       root: path.join(__dirname, '..'),
     },
