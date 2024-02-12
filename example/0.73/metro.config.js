50539810ee1754150b1f87d47f2ae29c2dd49c46
Lethe <37437842+0xlethe@users.noreply.github.com>
2024-02-12 13:34:43 +0900

    chore: add for example RN 0.73

--- example/0.73/metro.config.js
+++ example/0.73/metro.config.js
@@ -4,9 +4,9 @@ const defaultConfig = getDefaultConfig(__dirname);
 const path = require('path');
 const escape = require('escape-string-regexp');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
-const pak = require('../package.json');
+const pak = require('../../package.json');
 
-const root = path.resolve(__dirname, '..');
+const root = path.resolve(__dirname, '../..');
 
 const modules = Object.keys({
   ...pak.peerDependencies,
--- example/0.73/react-native.config.js
+++ example/0.73/react-native.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const pak = require('../package.json');
+const pak = require('../../package.json');
 
 module.exports = {
   dependencies: {
@@ -7,7 +7,7 @@ module.exports = {
       ? {'react-native-flipper': {platforms: {ios: null}}}
       : {}),
     [pak.name]: {
-      root: path.join(__dirname, '..'),
+      root: path.join(__dirname, '../..'),
     },
   },
 };
