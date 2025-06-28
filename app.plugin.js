ec0a295a71fab1f72779c458c931a8675a4b32fe
Lethe <lecheln00@gmail.com>
2025-06-28 11:42:05 +0900

    remove: annotation app.plugin.js

--- app.plugin.js
+++ app.plugin.js
@@ -1,16 +1,1 @@
-// import 'ts-node/register';
-
-// module.exports = (config) => {
-//   console.log('>', config);
-
-//   return {
-//     plugins: [['./plugins/withPlugin.js']],
-//   };
-// };
-// module.exports = require('./plugins/withPlugin.js');
-// Required for external files using TS
-// require('ts-node/register');
-
-// import withPlugin from './plugins/withPlugin';
-
 module.exports = require('./plugins/withPlugin.js');
