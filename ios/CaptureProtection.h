2d89d4eaf117b394c4d23b4b8410f902cc8d8f78
Lethe <37437842+wn-na@users.noreply.github.com>
2025-08-17 19:44:52 +0900

    feat: support ios new architecture

---
+++ ios/CaptureProtection.h
@@ -0,0 +1,11 @@
+#ifdef RCT_NEW_ARCH_ENABLED
+#import "generated/CaptureProtectionSpec/CaptureProtectionSpec.h"
+
+@interface CaptureProtection : NSObject <NativeCaptureProtectionSpec>
+#else
+#import <React/RCTBridgeModule.h>
+
+@interface CaptureProtection : NSObject <RCTBridgeModule>
+#endif
+
+@end
